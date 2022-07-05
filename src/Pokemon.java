0eb8de5999da66bd0e32dedcf3dfae771d59414c
IamLeila <beerleila10@gmail.com>
2022-07-05 13:43:17 +0200

    Create fight method

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,3 +1,5 @@
+import java.nio.file.FileStore;
+import java.util.Random;
 
 public class Pokemon {
     public String name;
@@ -17,7 +19,49 @@ public class Pokemon {
 
     public void PropertyPrintout() {
     System.out.println("Name: " + this.name + "\nLevel: " + this.level + "\nAttack: " + this.atk + "\nHealth: " + this.hp + "\nSpeed: " + this.spd);
+    }
 
+    public void Fight(Pokemon firstPokemon, Pokemon secondPokemon){
+        Random rd = new Random();
+        
+        firstPokemon.PropertyPrintout();
+        secondPokemon.PropertyPrintout();       
+        
+        while (firstPokemon.hp > 0 && secondPokemon.hp > 0) {
+
+
+            if (firstPokemon.spd > secondPokemon.spd) {
+
+                secondPokemon.hp = secondPokemon.hp - firstPokemon.atk;
+            }
+            else if (firstPokemon.spd < secondPokemon.spd) {
+
+                firstPokemon.hp = firstPokemon.hp - secondPokemon.hp;
+            }
+            else {               
+                boolean rnGesus = rd.nextBoolean();
+                
+                if (rnGesus) {
+
+                    secondPokemon.hp = secondPokemon.hp - firstPokemon.atk;
+                }
+                else {
+
+                    firstPokemon.hp = firstPokemon.hp - secondPokemon.hp;
+                }
+
+            }
+
+            firstPokemon.PropertyPrintout();
+            secondPokemon.PropertyPrintout();
+        }
+
+        if  (firstPokemon.hp > secondPokemon.hp) {
+            System.out.println("Winner: " + firstPokemon.name + "!");
+        }
+        else {
+            System.out.println("Winner :" + " " + secondPokemon.name + "!");
+        }
     }
 }
 
